9fba3bbddedded7b9f0e7bae75f8b821b1e51b17
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-01-31 23:50:03 +0100

    adding Loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { useState, useEffect } from 'react';
 import Characters from './Characters';
+import Loading from './Loading';
 
 const url = 'https://rickandmortyapi.com/api/character';
 
 const App = () => {
   const [characters, setCharacter] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const getCharacters = async () => {
+    setIsLoading(true);
     const response = await fetch(url);
     let characters = await response.json();
     characters = characters.results;
     setCharacter(characters);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getCharacters();
   }, []);
+
+  if (isLoading) {
+    return (
+      <main className='container loading-container'>
+        <Loading />
+      </main>
+    );
+  }
 
   return (
     <main className='container'>
